fb72ae73c8cf94fbe144a01bff2172350524fcf9
wh.jeong@jnpmedi.com <wh.jeong@jnpmedi.com>
2024-12-11 18:45:33 +0900

    init, split constants, build details api

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import streamlit as st
 from math import ceil
 from time import sleep
 from module.utils import convert_df, get_request
+from module.constants import STUDY_COLUMN_NAME, STUDY_DETAILS_COLUMN_NAME
 
-URL = 'http://apis.data.go.kr/1471000/MdcinClincTestInfoService02/getMdcinClincTestInfoList02'
+if 'clinical trials info df' not in st.session_state:
+    st.session_state['clinical trials info df'] = pd.DataFrame(columns=STUDY_COLUMN_NAME)
+
+# progress_bar
+progress_bar = st.progress(0)
 
 title = st.text_input('Title')
 date = st.text_input('Approval Date')
 
 if st.button('Search', key='serch_button'):
+    URL = 'http://apis.data.go.kr/1471000/MdcinClincTestInfoService02/getMdcinClincTestInfoList02'
     params = {
         'serviceKey': st.secrets['DECODED_API_KEY'],
         'clinic_exam_title': title,
@@ -31,9 +37,6 @@ if st.button('Search', key='serch_button'):
         total_count = response.json()['body']['totalCount']
         max_page = int(ceil(total_count) / params['numOfRows'])
         
-        # progress_bar
-        progress_bar = st.progress(0)
-
         # 100 건 이내인 경우
         if max_page == 1:
             for i in range(0, 100):
@@ -54,19 +57,22 @@ if st.button('Search', key='serch_button'):
                     initial_result.extend(response.json()['body']['items'])
 
         # 가장 마지막 Column 제거
-        df = pd.DataFrame(initial_result).iloc[:, :-1]
-        df.columns = ['Sponsor', 'IND Approval Date', 'Site Name', 'IP Name', 'Protocol Title', 'Phase']
+        df = pd.DataFrame(initial_result)
+        df.columns = STUDY_COLUMN_NAME
+
+        # session status에 저장
+        st.session_state['clinical trials info df'] = df
 
         # Exapmle Dataframe
-        st.dataframe(df)
+        st.dataframe(df.reset_index(drop=True))
 
         # Download Button
         st.download_button(
-            label="download_button",
+            label="Download",
+            key='clinical_trials_info_download_button',
             data=convert_df(df),
             file_name=f'clinical_trials_info.xlsx',
-            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        )
+            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
         # 상위 10개 Sponsor
         top10_trial_count_per_sponsor_df = df.groupby('Sponsor', as_index=False).count()[['Sponsor', 'Protocol Title']].nlargest(10, 'Protocol Title').sort_values('Protocol Title', ascending=True)
@@ -106,3 +112,70 @@ if st.button('Search', key='serch_button'):
         # fig2.update_xaxes(dtick=1)
 
         # st.plotly_chart(fig2)
+
+# Detail Search
+df = st.session_state['clinical trials info df']
+clinical_trial_id = st.text_input('Clinical Trial ID')
+clinical_trial_ids = df['Clinical Trial ID']
+
+if clinical_trial_id:
+    if max_page == 1:
+        for i in range(0, 100):
+            sleep(0.01)
+            progress_bar.progress(i, text='Processing')
+
+    if st.button(label='Search Details', key='search_details_button_for_single_ids'):
+        URL = 'http://apis.data.go.kr/1471000/ClncExamPlanDtlService2/getClncExamPlanDtlInq2'
+        params = {
+            'serviceKey': st.secrets['DECODED_API_KEY'],
+            'pageNo': 1,
+            'numOfRows': 100,
+            'type': 'json',
+            'CLNC_TEST_SN': clinical_trial_id
+            }
+
+        response = get_request(URL, params=params)
+        if response.status_code != 200:
+            st.warning(f'Error occured, status_code: {response.status_code}, response: {response.text}')
+
+        else:
+            temp_df = pd.DataFrame(response.json()['body']['items'])
+            temp_df.columns = STUDY_DETAILS_COLUMN_NAME
+            st.dataframe(temp_df)
+
+else:
+    clinical_trial_ids = df['Clinical Trial ID']
+    temp_list = []
+    if st.button(label='Search Details', key='search_details_button_for_multi_ids'):
+        for i, clinical_trial_id in enumerate(clinical_trial_ids, start=1):
+            progress_bar.progress(i / len(clinical_trial_ids), text='Processing')
+
+            URL = 'http://apis.data.go.kr/1471000/ClncExamPlanDtlService2/getClncExamPlanDtlInq2'
+            params = {
+                'serviceKey': st.secrets['DECODED_API_KEY'],
+                'pageNo': 1,
+                'numOfRows': 100,
+                'type': 'json',
+                'CLNC_TEST_SN': clinical_trial_id
+                }
+
+            response = get_request(URL, params=params)
+            if response.status_code != 200:
+                st.warning(f'Error occured, status_code: {response.status_code}, response: {response.text}')
+
+            else:
+                temp_list.append(pd.DataFrame(response.json()['body']['items']))
+            
+    
+        clinical_trials_details_info_df = pd.concat(temp_list)
+        clinical_trials_details_info_df.columns = STUDY_DETAILS_COLUMN_NAME
+        
+        st.dataframe(clinical_trials_details_info_df.reset_index(drop=True))
+
+        st.download_button(
+            label='Downlaod',
+            key="clinical_trials_details_info_excel_download_button",
+            data=convert_df(clinical_trials_details_info_df),
+            file_name=f'clinical_trials_details_info.xlsx',
+            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
---
+++ module/constants.py
@@ -0,0 +1,17 @@
+STUDY_COLUMN_NAME = ['Sponsor', 'IND Approval Date', 'Site Name', 'IP Name', 'Protocol Title', 'Phase', 'Clinical Trial ID']
+STUDY_DETAILS_COLUMN_NAME = [
+    'Sponsor', 'President of the Sponsor', 'Address of the Sponsor', 
+    'Original Developer of the IP', 'Nationality of the Original Developer', 
+    'Last (Reivision) IND Approval Date', 'First IND Approval Date', 
+    'Protocol Title (Korean)', 'Protocol Title (English)', 'Protocol Purpose',
+    'Study Purpose', 'Development Region', 'Development Region (Details)',
+    'Target Disease Category', 'Target Disease Category (Details)', 'Target Disease Category (Direct Input)',
+    'Target Disease Name', 'Product Name/Code of the IP', 'Ingredient Name/Code of the IP',
+    'Domestic Approval', 'Active Control Arm Medication', 'First-in-human Study', 'Control Arm Medication',
+    'Placebo', 'Product Name of the Control Arm Medication', 'Form of the IP', 'Phase',
+    'Study Design', 'Study Period', 'Number of Target Subject (In Korea)',
+    'Randomization', 'Blind', 'Number of Arm', 'Date of First Subject In', 'Date of Last Subject In',
+    'Date of Last Subject Out', 'Actual Date of the Study Completion', 'Submit Date of the Clinical Study Report',
+    'Primary Efficacy Endpoint', 'Secondary Efficacy Endpoint', 'Period of the IP Administration',
+    'Method of the IP Administration', 'Study Status', 'Clinical Trial ID'
+]
